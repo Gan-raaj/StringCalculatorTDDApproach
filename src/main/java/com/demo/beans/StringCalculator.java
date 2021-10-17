46e8ed6d2c2294a73931c897dfccc958c69357c8
[ganesh chuadhari] <[ganraaj1777@gmail.com]>
2021-10-17 18:06:14 +0530

    initial commit

---
+++ src/main/java/com/demo/beans/StringCalculator.java
@@ -0,0 +1,16 @@
+package com.demo.beans;
+
+public class StringCalculator {
+	
+	public int add(String number) {
+		if(number == "") {
+			return 0;
+		}
+		return 0;
+		//number = removalOfDelimiters(number);
+		//checkForNegativeNumbers(number);
+		//number = removeLargeNumbers(number);
+		//return sum(splitString(number));
+	}
+
+	}
---
+++ src/test/java/com/demo/test/TestStringCal.java
@@ -0,0 +1,23 @@
+package com.demo.test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.demo.beans.StringCalculator;
+
+public class TestStringCal {
+	StringCalculator sc;
+	
+	@Before
+	public void setUp() {
+		sc = new StringCalculator();
+	}
+	
+	@Test
+	public void testForZero() {
+		assertEquals(0,sc.add(""));
+	}
+	
+	}
